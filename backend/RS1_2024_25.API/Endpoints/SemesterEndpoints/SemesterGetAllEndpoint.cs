fbfe980beb36d421d574830facd8ed6de9b35d95
cheZA99 <amerzelic08@gmail.com>
2026-02-02 22:57:23 +0100

    complete implementation of three exam tasks

---
+++ backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterGetAllEndpoint.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic;
+using RS1_2024_25.API.Helper;
+using RS1_2024_25.API.Helper.Api;
+using RS1_2024_25.API.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+using static RS1_2024_25.API.Endpoints.ChatEndpoints.ChatUserGetEndpoint;
+using static RS1_2024_25.API.Endpoints.SemesterEndpoints.SemesterGetAllEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.SemesterEndpoints;
+
+[Route("students")]
+[MyAuthorization(isAdmin: true, isManager: false)]
+public class SemesterGetAllEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<SemesterGetAllRequest>
+    .WithResult<MyPagedList<SemesterGetAllResponse>>
+{
+    [HttpGet("{studentId}/semesters/filter")]
+
+    public override async Task<MyPagedList<SemesterGetAllResponse>> HandleAsync([FromQuery] SemesterGetAllRequest request, CancellationToken cancellationToken = default)
+    {
+        var studentId = int.Parse(HttpContext.Request.RouteValues["studentId"]!.ToString()!);
+        IQueryable<SemesterQueryResult> query;
+
+        if (request.Status == "active")
+        {
+            query = db.Semesters
+                .Include(p => p.AcademicYear)
+                .IgnoreQueryFilters()
+                .Where(x => !x.IsDeleted && x.StudentId == studentId)
+                .Select(p => new SemesterQueryResult
+                {
+                    ID = p.ID,
+                    AcademicYearDesc = p.AcademicYear.Description,
+                    StudyYear = p.StudyYear,
+                    EnrollmentDate = p.EnrollmentDate,
+                    IsRenewal = p.IsRenewal,
+                    TuitionFee = p.TuitionFee,
+                    IsDeleted = p.IsDeleted,
+                });
+        }
+        else if (request.Status == "deleted")
+        {
+            query = db.Semesters
+               .Include(p => p.AcademicYear)
+              .IgnoreQueryFilters()
+               .Where(x => x.IsDeleted && x.StudentId == studentId)
+               .Select(p => new SemesterQueryResult
+               {
+                   ID = p.ID,
+                   AcademicYearDesc = p.AcademicYear.Description,
+                   StudyYear = p.StudyYear,
+                   EnrollmentDate = p.EnrollmentDate,
+                   IsRenewal = p.IsRenewal,
+                   TuitionFee = p.TuitionFee,
+                   IsDeleted = p.IsDeleted,
+               });
+        }
+        else
+        {
+            query = db.Semesters
+              .Include(p => p.AcademicYear)
+              .IgnoreQueryFilters()
+              .Where(x => x.StudentId == studentId)
+              .Select(p => new SemesterQueryResult
+              {
+                  ID = p.ID,
+                  AcademicYearDesc = p.AcademicYear.Description,
+                  StudyYear = p.StudyYear,
+                  EnrollmentDate = p.EnrollmentDate,
+                  IsRenewal = p.IsRenewal,
+                  TuitionFee = p.TuitionFee,
+                  IsDeleted = p.IsDeleted,
+              });
+        }
+
+        if (!string.IsNullOrEmpty(request.Q))
+        {
+            query = query.Where(u =>
+                (u.AcademicYearDesc).Contains(request.Q));
+        }
+
+        // Kreiranje paginiranog rezultata
+        var result = await MyPagedList<SemesterGetAllResponse>.CreateAsync(query, request, cancellationToken);
+
+        return result;
+    }
+
+    // DTO za zahtjev s podrškom za paginaciju i filtriranje
+    public class SemesterGetAllRequest : MyPagedRequest
+    {
+        public string? Q { get; set; } = string.Empty; // Tekstualni upit za pretragu
+        public string? Status { get; set; }
+
+    }
+
+    // DTO za odgovor
+    public class SemesterGetAllResponse
+    {
+        public required int ID { get; set; }
+        public string AcademicYearDesc { get; set; }
+        public int StudyYear { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public float TuitionFee { get; set; }
+        public bool IsRenewal { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+    private class SemesterQueryResult : SemesterGetAllResponse { }
+
+}
---
+++ backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterUpdateOrInsertEndpoint.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Enums;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic;
+using RS1_2024_25.API.Helper.Api;
+using RS1_2024_25.API.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+using static RS1_2024_25.API.Endpoints.SemesterEndpoints.SemesterUpdateOrInsertEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.SemesterEndpoints;
+
+[Route("students")]
+
+[MyAuthorization(isAdmin: true, isManager: false)]
+public class SemesterUpdateOrInsertEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<SemesterUpdateOrInsertRequest>
+    .WithActionResult<int>
+{
+    [HttpPost("{studentId}/semesters")]
+    public override async Task<ActionResult<int>> HandleAsync(
+        [FromBody] SemesterUpdateOrInsertRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        // Check if it's an insert or update operation
+        bool isInsert = request.ID == null || request.ID == 0;
+        Semester? semester;
+
+        if (isInsert)
+        {
+            semester = new Semester
+            {
+            };
+            db.Add(semester);
+        }
+        else
+        {
+            // Update operation: retrieve the existing semester
+            semester = await db.Semesters
+                .SingleOrDefaultAsync(x => x.ID == request.ID, cancellationToken);
+
+            if (semester == null)
+            {
+                return NotFound("Semester not found");
+            }
+        }
+
+        // Set common properties for both insert and update
+      
+        semester.AcademicYearId = request.AcademicYearId;
+        semester.StudyYear = request.StudyYear;
+        semester.EnrollmentDate = request.EnrollmentDate;
+
+        var authInfo = MyAuthServiceHelper
+     .GetAuthInfoFromRequest(db, HttpContext.RequestServices.GetService<IHttpContextAccessor>()!);
+        var studentId = int.Parse(
+       HttpContext.Request.RouteValues["studentId"]!.ToString()!);
+
+        semester.RecordedById = authInfo.UserId;
+        semester.TenantId = authInfo.TenantId;
+        semester.StudentId = studentId;
+
+        var count = await db.Semesters
+      .CountAsync(x => x.StudentId == studentId &&
+                       x.StudyYear == semester.StudyYear,
+                  cancellationToken);
+
+        if (count == 1)
+        {
+            semester.IsRenewal = true;
+            semester.TuitionFee = 400;
+        }
+        else if (count >= 2)
+        {
+            semester.IsRenewal = true;
+            semester.TuitionFee = 500;
+        }
+        else
+        {
+            semester.IsRenewal = false;
+            semester.TuitionFee = 1800;
+        }
+
+        if(db.Semesters.ToList().Exists(x=>x.StudentId == studentId && x.AcademicYearId == request.AcademicYearId && !x.IsDeleted))
+        {
+            throw new Exception("Akademska godina za studenta vec postoji!");
+        }
+
+        // Save changes to the database
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Ok(semester.ID); // Return the ID of the semester
+    }
+
+    public class SemesterUpdateOrInsertRequest
+    {
+        public int? ID { get; set; } // Nullable to allow null for insert operations
+        public int AcademicYearId { get; set; }
+        public int StudyYear { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
